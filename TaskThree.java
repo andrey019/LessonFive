2bf823b152afcbd6aeea223fd1912b52e97d958d
andrey019 <andrey019@gmail.com>
2016-03-18 16:01:30 +0200

    Added files via upload

---
+++ TaskThree.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ Написать ф-ю, которая принимает на вход массив чисел и
+ возвращает его длину в байтах как результат
+ */
+public class TaskThree {
+    private static int sizeOfArray(int[] intarray){
+        int result = intarray.length * 4;
+        return result;
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("State the array size: ");
+        int arraysize = scan.nextInt();
+        int[] intarray = RandIntArr.arrayRand(arraysize, 100);
+
+        System.out.println("Created array: " + Arrays.toString(intarray));
+        System.out.println("Result: array size is " + sizeOfArray(intarray) + " bytes");
+    }
+}
